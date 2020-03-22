37bc5eeebbd83bddf40e7dd3fc6a06b9bbb67e14
Sotiris Niarchos <sot.niarchos@gmail.com>
2020-03-22 13:12:53 +0200

    playing around with CI

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='rvg',
     version='0.1',
     description='random values generator',
-    long_descrption=open('README.md', 'r').read(),
+    long_description=open('README.md', 'r').read(),
     author='Sotiris Niarchos',
     author_email='sot.niarchos@gmail.com',
     # url='',
