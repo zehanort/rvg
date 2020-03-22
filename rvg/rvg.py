fixed numpy overflow errors and respective tests

rvg/rvg.py
@@ -27,20 +27,35 @@ class NumPyRVG:
                     stacklevel=2
                 )
 
-    def scalar_or_array(self, rvg, args, dtype, length):
+    def scalar_or_array(self, rvg, limits, dtype, length):
+
+        # fix limits depending on dtype
+        if not hasattr(dtype, 'fields'):
+            if np.issubdtype(dtype, np.floating):
+                actual_dtype_limits = np.finfo(dtype)
+            else:
+                actual_dtype_limits = np.iinfo(dtype)
+            actual_dtype_limits = actual_dtype_limits.min, actual_dtype_limits.max
+            rectified_limits = [
+                max(limits[0], actual_dtype_limits[0]),
+                min(limits[1], actual_dtype_limits[1])
+            ]
+            if rectified_limits != sorted(rectified_limits):
+                raise ValueError(f'unproper limits {limits} for the requested dtype {dtype} with actual limits {actual_dtype_limits}')
+            limits = rectified_limits
 
         # scalar requested
         if not length:
             try:
-                val = rvg(*args)
+                val = rvg(*limits)
                 return dtype(val)
             except TypeError:
                 return val
             except ValueError:
-                raise ValueError(f'Can not generate unsigned value with upper bound equal to {self.b}')
+                raise ValueError(f'unproper limits {limits} for the requested dtype')
 
         # array requested
-        vals = [rvg(*args) for _ in range(length)]
+        vals = [rvg(*limits) for _ in range(length)]
         try:
             return np.array(map(dtype, vals), dtype=dtype)
         except TypeError:

tests/test_NumPyRVG_class.py
@@ -30,8 +30,9 @@ def create_struct_dtype():
 def test_scalar_dtypes():
     rand = NumPyRVG(1000)
     for dtype in dtypes:
-        val = rand(dtype)
-        assert type(val) == dtype
+        for _ in range(50):
+            val = rand(dtype)
+            assert type(val) == dtype
 
 def test_array_dtypes():
     rand = NumPyRVG(1000)
@@ -123,8 +124,17 @@ def test_a_b_limits_errors():
     with pytest.warns(Warning, match=f'value {b} for argument `b` will cause a runtime error if generation of values of unsigned type is attempted'):
         rand = NumPyRVG(a, b)
 
-    with pytest.raises(ValueError, match=f'Can not generate unsigned value with upper bound equal to {b}') as e:
+    with pytest.raises(ValueError) as e:
         rand(np.uint32)
+        assert str(e.value).startswith('unproper limits')
+
+def test_a_b_limits_improper_usage():
+
+    a, b = 1, 1000
+    rand = NumPyRVG(a, b)
+
+    vals = rand(np.int8, length=100)
+    assert (vals >= a).all() and (vals <= b).all()
 
 def test_a_b_limits_proper_usage():
 